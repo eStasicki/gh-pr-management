4203871bad16bef35fd2fec5bbbd0988c2dc5601
Ernest Stasicki <ernest.stasicki@monogo.pl>
2025-10-08 01:33:52 +0200

    update paths for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,25 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
-		}),
-		paths: {
-			base: '/gh-pr-management',
-		},
-		prerender: {
-			handleHttpError: 'warn'
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+      precompress: false,
+      strict: false,
+    }),
+    paths: { base: dev ? "" : "/gh-pr-management" },
+    prerender: {
+      handleHttpError: "warn",
+    },
+  },
 };
 
 export default config;
