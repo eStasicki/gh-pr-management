dceb9ad9bfa19057e20b2f49fcadb821995af2d8
Ernest Stasicki <ernest.stasicki@monogo.pl>
2025-10-08 02:25:59 +0200

    fix(svelte-config): remove base path for production environment

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
@@ -16,7 +14,7 @@ const config = {
       strict: false,
     }),
     paths: {
-      base: process.env.NODE_ENV === "development" ? "" : "/gh-pr-management",
+      base: process.env.NODE_ENV === "development" ? "" : "",
     },
     prerender: {
       handleHttpError: "warn",
