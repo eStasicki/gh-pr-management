5ebf8170cb928062af459a69be097ba7140c0791
Ernest Stasicki <estasicki@gmail.com>
2025-10-10 10:32:44 +0200

    feat(Auth): enhance authentication flow by dynamically importing Supabase Auth components and adding loading state; ensure proper initialization in browser environment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,19 @@
   import "../app.css";
   import { initializeAuth } from "$lib/stores/supabaseAuth";
   import { onMount } from "svelte";
+  import { browser } from "$app/environment";
   import { inject } from "@vercel/analytics";
   import { injectSpeedInsights } from "@vercel/speed-insights";
 
-  onMount(() => {
-    initializeAuth();
-    inject();
-    injectSpeedInsights();
+  onMount(async () => {
+    if (browser) {
+      await initializeAuth();
+
+      setTimeout(() => {
+        inject();
+        injectSpeedInsights();
+      }, 100);
+    }
   });
 </script>
 
